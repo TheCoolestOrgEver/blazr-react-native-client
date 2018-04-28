544032b4c8c6d0e9d62be24e647225cc4611f0cd
cduica <crisd0000@gmail.com>
2018-04-28 15:38:12 -0400

    fixed some stuff

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import firebase from 'firebase';
 import reducers from './reducers'
 import LoginForm from './components/LoginForm';
 import Router from './Router';
+var Config = require('../config.json')
 
 class App extends Component {
 
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: "AIzaSyDBugnZ_UZIrLV3QNr1wWKDIm1Bx7BpbqI",
-      authDomain: "blazr-32999.firebaseapp.com",
-      databaseURL: "https://blazr-32999.firebaseio.com",
-      projectId: "blazr-32999",
-      storageBucket: "blazr-32999.appspot.com",
-      messagingSenderId: "466611530510"
+      apiKey: Config.API_KEY,
+      authDomain: Config.AUTH_DOMAIN,
+      databaseURL: Config.DATABASE_URL,
+      projectId: Config.PROJECT_ID,
+      storageBucket: Config.STORAGE_BUCKET,
+      messagingSenderId: Config.MESSAGING_SENDER_ID
     });
   }
 
